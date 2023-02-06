3f17c9c7f687cfb28c921ad501ee6205e732c67d
nrthokala <89919020+nrthokala@users.noreply.github.com>
2023-02-06 16:33:18 -0500

    Update acount.py

--- mrlab/q1/acount.py
+++ mrlab/q1/acount.py
@@ -1,14 +1,15 @@
 from mrjob.job import MRJob   # MRJob version
 
 # Change the class name!!
-class WordCount(MRJob):  #MRJob version
+class Acount(MRJob):  #MRJob version
     def mapper(self, key, line):
         words = line.split()
         for w in words:
-            yield (w, 1)
+            if "a" in words:
+                yield (w, 1)
 
     def reducer(self, key, values):
         yield (key, sum(values))
 
 if __name__ == '__main__':
-    WordCount.run()   # MRJob version
+    Acount.run()   # MRJob version

77f6b191f2a26c373d41aad6c056ba53e7e60565
nrthokala <89919020+nrthokala@users.noreply.github.com>
2023-02-06 16:54:42 -0500

    Update acount.py

--- mrlab/q1/acount.py
+++ mrlab/q1/acount.py
@@ -1,15 +1,18 @@
-from mrjob.job import MRJob   # MRJob version
+from HDFS import wap   
 
 # Change the class name!!
-class Acount(MRJob):  #MRJob version
+class Acount(wap):  
     def mapper(self, key, line):
         words = line.split()
+        count = 0
         for w in words:
             if "a" in words:
                 yield (w, 1)
+                count = count + 1
+        yield count
 
     def reducer(self, key, values):
         yield (key, sum(values))
 
 if __name__ == '__main__':
-    Acount.run()   # MRJob version
+    Acount.run()   
